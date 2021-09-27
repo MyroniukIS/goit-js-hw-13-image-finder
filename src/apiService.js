c9ee02e2572744fd2315ac89a22dc5ba169f342f
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-09-27 21:54:49 +0300

    all in one file

--- src/apiService.js
+++ src/apiService.js
@@ -5,9 +5,9 @@ import '@pnotify/core/dist/BrightTheme.css';
 defaultModules.set(PNotifyMobile, {});
 
 import refs from './refs.js'
-import throwErrorInvalid from './index.js'
-import createMarkup from './index.js'
-import onSmoothScroll from './index.js'
+// import throwErrorInvalid from './index.js'
+// import createMarkup from './index.js'
+// import onSmoothScroll from './index.js'
 
 const myStack = new Stack({
     dir1: 'up',
@@ -17,7 +17,7 @@ const myStack = new Stack({
     overlayClose: true
 });
 
-const API_key = '23557940-e0a9cdf2e70b178fd7f1f33b8';
+
 
 // export default function fetchImages(searchQuery) {
 
@@ -34,44 +34,3 @@ const API_key = '23557940-e0a9cdf2e70b178fd7f1f33b8';
 //     }
 // }
 
-
-export default {
-    searchQuery: '',
-    page: 1,
-
-    fetchImages() {
-        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.guery}&page=${this.page}&per_page=12&key=${API_key}`;
-        return fetch(url)
-            .then(response => {
-                if (response.ok) {
-                return response.json();
-                }
-                if (response.status === 404) {
-                   throwErrorInvalid();
-            }
-            })
-            .then(({ hits }) => {
-                if (!hits) {
-                return;
-                }
-                createMarkup(hits);
-                onSmoothScroll();
-                this.incrPage();
-                return hits;
-            })
-            .catch(error => console.log(error));
-    },
-
-    incrPage() {
-        this.page += 1;
-    },
-    dafaultPage() {
-        this.page = 1;
-    },
-    get query() {
-        return this.searchQuery;
-    },
-    set query(value) {
-        this.searchQuery = value;
-    },
-};
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import '@pnotify/core/dist/BrightTheme.css';
 defaultModules.set(PNotifyMobile, {});
 
 import debounce from 'lodash/debounce';
-import objFetchImg from './apiService.js'
+// import objFetchImg from './apiService.js'
 import refs from './refs.js'
 import { template } from 'handlebars';
 import cardMarkup from './templates/cardMarkup.hbs'
@@ -17,6 +17,48 @@ const myStack = new Stack({
     modal: true,
     overlayClose: true
 });
+//========================================================
+const API_key = '23557940-e0a9cdf2e70b178fd7f1f33b8';
+const objFetchImg = {
+    searchQuery: '',
+    page: 1,
+
+    fetchImages() {
+        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.guery}&page=${this.page}&per_page=12&key=${API_key}`;
+        return fetch(url)
+            .then(response => {
+                if (response.ok) {
+                return response.json();
+                }
+                if (response.status === 404) {
+                   throwErrorInvalid();
+            }
+            })
+            .then(({ hits }) => {
+                if (!hits) {
+                return;
+                }
+                createMarkup(hits);
+                onSmoothScroll();
+                this.incrPage();
+                return hits;
+            })
+            .catch(error => console.log(error));
+    },
+
+    incrPage() {
+        this.page += 1;
+    },
+    dafaultPage() {
+        this.page = 1;
+    },
+    get query() {
+        return this.searchQuery;
+    },
+    set query(value) {
+        this.searchQuery = value;
+    },
+};
 //========================================================
 
 refs.button.classList.add('hide');
@@ -53,7 +95,7 @@ function onEnterInput(e) {
 //         });
 // }
 
-export function createMarkup(hits) {
+function createMarkup(hits) {
         if (hits.length === 0) {
         throwErrorInvalid();
             return;
@@ -61,14 +103,14 @@ export function createMarkup(hits) {
     refs.gallery.insertAdjacentHTML('beforeend', cardMarkup(hits));
 }
 
-export function onSmoothScroll() {
+function onSmoothScroll() {
     refs.gallery.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
 });
 }
 
-export function throwErrorInvalid() {
+function throwErrorInvalid() {
     refs.mainInput.disabled = true;
             error({
              text: "Please enter valid query!",
