b1eb411d01187971043f31f44441011035ba57c6
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-09-28 21:10:35 +0300

    create object with logic of fetch

--- src/apiService.js
+++ src/apiService.js
@@ -4,10 +4,9 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 defaultModules.set(PNotifyMobile, {});
 
-// import refs from './refs.js'
-// import throwErrorInvalid from './index.js'
-// import createMarkup from './index.js'
-// import onSmoothScroll from './index.js'
+import throwErrorInvalid from './index.js'
+import createMarkup from './index.js'
+import onSmoothScroll from './index.js'
 
 const myStack = new Stack({
     dir1: 'up',
@@ -16,3 +15,47 @@ const myStack = new Stack({
     modal: true,
     overlayClose: true
 });
+
+//========================================================
+
+const API_key = '23557940-e0a9cdf2e70b178fd7f1f33b8';
+export default {
+    searchQuery: '',
+    page: 1,
+
+    fetchImages() {
+        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_key}`;
+        return fetch(url)
+            .then(response => {
+                if (response.ok) {
+                return response.json();
+                }
+                if (response.status === 404) {
+                   throwErrorInvalid();
+            }
+            })
+            .then(({ hits }) => {
+                if (!hits) {
+                return;
+                }
+                createMarkup(hits);
+                onSmoothScroll();
+                this.incrPage();
+                return hits;
+            })
+            .catch(error => console.log(error));
+    },
+
+    incrPage() {
+        this.page += 1;
+    },
+    dafaultPage() {
+        this.page = 1;
+    },
+    get query() {
+        return this.searchQuery;
+    },
+    set query(value) {
+        this.searchQuery = value;
+    },
+};
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 defaultModules.set(PNotifyMobile, {});
 
-import debounce from 'lodash/debounce';
-// import objFetchImg from './apiService.js'
-import refs from './refs.js'
 import { template } from 'handlebars';
+import debounce from 'lodash/debounce';
+import objFetchImg from './apiService.js'
+import refs from './refs.js'
 import cardMarkup from './templates/cardMarkup.hbs'
 
 const myStack = new Stack({
@@ -18,91 +18,50 @@ const myStack = new Stack({
     overlayClose: true
 });
 
-//========================================================
 
-const API_key = '23557940-e0a9cdf2e70b178fd7f1f33b8';
-const objFetchImg = {
-    searchQuery: '',
-    page: 1,
-
-    fetchImages() {
-        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.guery}&page=${this.page}&per_page=12&key=${API_key}`;
-        return fetch(url)
-            .then(response => {
-                if (response.ok) {
-                return response.json();
-                }
-                if (response.status === 404) {
-                   throwErrorInvalid();
-            }
-            })
-            .then(({ hits }) => {
-                if (!hits) {
-                return;
-                }
-                createMarkup(hits);
-                onSmoothScroll();
-                this.incrPage();
-                return hits;
-            })
-            .catch(error => console.log(error));
-    },
-
-    incrPage() {
-        this.page += 1;
-    },
-    dafaultPage() {
-        this.page = 1;
-    },
-    get query() {
-        return this.searchQuery;
-    },
-    set query(value) {
-        this.searchQuery = value;
-    },
-};
 
 //========================================================
 
 refs.button.classList.add('hide');
 refs.mainInput.addEventListener('input', debounce(onEnterInput, 1500));
 refs.button.addEventListener('click', () => {
-    objFetchImg.fetchImages()
+    objFetchImg.fetchImages();
 });
 
 function onEnterInput(e) {
+    objFetchImg.dafaultPage();
     objFetchImg.query = e.target.value.trim();
-    if (objFetchImg.query.length < 1) {
+    if (objFetchImg.searchQuery.length < 1) {
          throwErrorInvalid();
             return;
         }   
     refs.gallery.innerHTML = '';
     objFetchImg.fetchImages();
-    refs.button.classList.remove('hide');
  }
 
-function createMarkup(hits) {
-        if (hits.length === 0) {
+export function createMarkup(hits) {
+    if (hits.length === 0) {
+            refs.button.classList.add('hide');
             throwErrorInvalid();
-            // objFetchImg.dafaultPage();
             return;
     }
+    refs.button.classList.remove('hide');
     refs.gallery.insertAdjacentHTML('beforeend', cardMarkup(hits));
 }
 
-function onSmoothScroll() {
+export function onSmoothScroll() {
     refs.gallery.scrollIntoView({
     behavior: 'smooth',
     block: 'end',
 });
 }
 
-function throwErrorInvalid() {
+export function throwErrorInvalid() {
     refs.mainInput.disabled = true;
             error({
              text: "Please enter valid query!",
              stack: myStack
             });
     refs.mainInput.disabled = false;
-        refs.mainInput.value = '';
+    refs.mainInput.value = '';
  }
